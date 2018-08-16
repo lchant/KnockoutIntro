d86d150beaee5988f67205003a69f0130927fafb
lchant <lchant@hearst.com>
2018-08-16 14:41:33 +0100

    Started afresh with empty web app

--- GettingStartedWithKnockout/Scripts/home.js
+++
@@ -1,8 +0,0 @@
-﻿/// <reference path= "/scripts/knockout-2.0.0.debug.js" />
-
-$(function () {
-    var viewModel = {
-        firstName: ko.observable("John")
-    };
-    ko.applyBindings(viewModel);
-})
